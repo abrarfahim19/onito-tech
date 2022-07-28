51c0e2f24186d830373e76ae11250f288f216e54
abrarfahim19 <fahimpatwari19@gmail.com>
2022-07-28 12:43:14 -0400

    Dark Mode added for readability

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import ReceiptsView from "./features/ReceiptsView";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 function App() {
+  const themeValues = ["dark", "cupcake", "Bumblebee"];
+  useEffect(() => {
+    themeChange(false);
+  });
   return (
     <div>
       <Routes>
